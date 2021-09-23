4dc0e27d9e52a0a695e42679f46f82f14cb7d48a
Rakshit Sharma <rashu.sharma14@gmail.com>
2021-09-24 02:10:59 +0530

    Razor Pay integration done

---
+++ src/main/java/com/startup/goHappy/integrations/service/RazorPayService.java
@@ -0,0 +1,52 @@
+package com.startup.goHappy.integrations.service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSONObject;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import com.startup.goHappy.integrations.model.ZoomMeetingObjectDTO;
+import com.startup.goHappy.integrations.model.ZoomMeetingSettingsDTO;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+
+
+@Service
+public class RazorPayService {
+	
+
+	@Value("${razorPay.apiKey}")
+    private String razorPayApiKey;
+	@Value("${razorPay.apiSecret}")
+    private String razorPayApiSecret;
+
+	public void pay() throws RazorpayException {
+		RazorpayClient razorpayClient = new RazorpayClient(razorPayApiKey, razorPayApiSecret);
+		org.json.JSONObject options = new org.json.JSONObject();
+		options.put("amount", 5000);
+		options.put("currency", "INR");
+		options.put("receipt", "txn_123456");
+		Order order = razorpayClient.Orders.create(options);
+//		Order order = razorpayClient.Orders.fetch("order_id");
+		System.out.println(order);
+    }
+	
+    
+
+}
