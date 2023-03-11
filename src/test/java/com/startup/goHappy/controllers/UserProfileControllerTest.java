46062c588dfddbf1d8ef15576c5cb58bca347520
Mohammed Ibrahim Shariff <misharif@uwaterloo.ca>
2023-03-11 13:23:11 -0500

    Adding test case for getUserByEmail

--- src/test/java/com/startup/goHappy/controllers/UserProfileControllerTest.java
+++ src/test/java/com/startup/goHappy/controllers/UserProfileControllerTest.java
@@ -8,6 +8,7 @@ import com.startup.goHappy.entities.model.UserProfile;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -39,29 +40,50 @@ class UserProfileControllerTest {
     @MockBean UserProfileController userProfileService;
 
     private Gson gson = new Gson();
+    static UserProfile user1 = new UserProfile();
+    static UserProfile user2 = new UserProfile();
+
+    static {
+        user1.setId("1234");
+        user1.setAge("23");
+        user1.setEmail("test@gmail.com");
+        user1.setName("abc");
+        user1.setPhone("123456789");
+        user1.setDob("1/12/1999");
+        user1.setSessionsAttended("23");
+        user1.setDateOfJoining("1/2/2022");
+        user1.setSelfInviteCode("23");
+        user1.setMembership("ewsa");
+        user1.setProfileImage("bacsfds");
+        user1.setSelfInviteCode("cnsdh");
+        user1.setGoogleSignIn(true);
+        user1.setPassword("adiufhnsdf");
+        user1.setLastPaymentDate("2/2/2022");
+        user1.setLastPaymentAmount(200);
+
+        user2.setId("1235");
+        user2.setAge("25");
+        user2.setEmail("test1@gmail.com");
+        user2.setName("Ibu");
+        user2.setPhone("9876579874");
+        user2.setDob("1/12/1970");
+        user2.setSessionsAttended("9");
+        user2.setDateOfJoining("11/2/2021");
+        user2.setSelfInviteCode("xdjif3");
+        user2.setMembership("sdkjv");
+        user2.setProfileImage("ksdjf");
+        user2.setSelfInviteCode("dkshfdio");
+        user2.setGoogleSignIn(false);
+        user2.setPassword("dfkjdin");
+        user2.setLastPaymentDate("2/12/2022");
+        user2.setLastPaymentAmount(500);
+    }
     @Test
     public void testFindAll() throws Exception {
 
         String URI = "/user/findAll";
 
         List<UserProfile> users = new ArrayList<UserProfile>();
-        UserProfile user1 = new UserProfile();
-        user1.setId("1234");
-        user1.setAge("23");
-        user1.setEmail("test@gmail.com");
-        user1.setName("abc");
-        user1.setPhone("123456789");
-//        user1.setDob("1/12/1999");
-//        user1.setSessionsAttended("23");
-//        user1.setDateOfJoining("1/2/2022");
-//        user1.setSelfInviteCode("23");
-//        user1.setMembership("ewsa");
-
-        UserProfile user2 = new UserProfile();
-        user2.setId("1235");
-        user2.setAge("25");
-        user2.setEmail("test1@gmail.com");
-
         users.add(user1);
         users.add(user2);
 
@@ -75,66 +97,35 @@ class UserProfileControllerTest {
 
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
+        String data = result.getResponse().getContentAsString();
+
+        JSONAssert.assertEquals(data, String.valueOf(output), false);
+
+        assertEquals(result.getResponse().getStatus(), 200);
+    }
+    @Test
+    public void getUserByEmailTest() throws Exception{
+        String URI = "/user/getUserByEmail";
+
+        JSONObject input = new JSONObject();
+        input.put("email", "test@gmail.com");
+
+        JSONObject output = new JSONObject();
+        output.put("user", user1);
+
+        Mockito.when(userProfileService.getUserByEmail(input)).thenReturn(output);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(URI)
+                .accept(MediaType.APPLICATION_JSON).content(String.valueOf(input))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
         String outputStr = result.getResponse().getContentAsString();
 
-        JsonObject jsonObject1 = gson.fromJson(String.valueOf(outputStr), JsonObject.class);
+        JSONAssert.assertEquals(outputStr, String.valueOf(output), false);
 
-        jsonObject1 = sortJsonObjectByKeys(jsonObject1);
-
-
-        JsonObject jsonObject = gson.fromJson(String.valueOf(outputStr), JsonObject.class);
-
-        jsonObject1 = sortJsonObjectByKeys(jsonObject1);
-
-        assertThat(String.valueOf(jsonObject1)).isEqualTo(String.valueOf(outputStr));
         assertEquals(result.getResponse().getStatus(), 200);
-
     }
-
-//    @Test
-//    public void getUserByEmailTest() throws Exception{
-//        String URI = "/user/getUserByEmail";
-//
-//        UserProfile user1 = new UserProfile();
-//        user1.setId("1234");
-//        user1.setAge("23");
-//        user1.setEmail("test@gmail.com");
-//        user1.setName("abc");
-//        user1.setPhone("123456789");
-//
-//        JSONObject input = new JSONObject();
-//        input.put("email", "test@gmail.com");
-//
-//        System.out.println(input);
-//        JSONObject output = new JSONObject();
-//        output.put("user", user1);
-//
-//        Mockito.when(userProfileService.getUserByEmail(input)).thenReturn(output);
-//
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(URI).accept(
-//                MediaType.APPLICATION_JSON);
-//
-//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-//
-//        String outputStr = result.getResponse().getContentAsString();
-//        System.out.println(outputStr);
-//
-//        JsonObject jsonObject1 = gson.fromJson(outputStr, JsonObject.class);
-//
-//        jsonObject1 = sortJsonObjectByKeys(jsonObject1);
-//
-//        assertThat(outputStr).isEqualTo(String.valueOf(jsonObject1));
-//        assertEquals(result.getResponse().getStatus(), 200);
-//    }
-    private JsonObject sortJsonObjectByKeys(JsonObject jsonObject) {
-        JsonObject sortedJsonObject = new JsonObject();
-        jsonObject.entrySet().stream()
-                .sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
-                .forEach(e -> sortedJsonObject.add(e.getKey(), e.getValue()));
-        return sortedJsonObject;
-    }
-
-
-
-
 }
