00b3a8d3dd11d04f9503f19f348b7f326db4f718
mehul1101 <mehul@drupsc.com>
2024-07-16 14:46:29 +0530

    Initial Commit
    Add OperationsTeamService
    
    Add Jwt dependencies
    
    Add Jwt important methods
    
    Add Jwt filter chaining
    
    Add post mapping to authenticate and generate token
    
    JWT fixed and working

---
+++ src/main/java/com/startup/goHappy/utils/JwtFilter.java
@@ -0,0 +1,48 @@
+package com.startup.goHappy.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (request.getServletPath().equals("/authenticate")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = extractToken(request);
+        if (token != null && jwtUtil.validateToken(token)) {
+            SecurityContextHolder.getContext().setAuthentication(
+                    new UsernamePasswordAuthenticationToken(null, null, null));
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized: Invalid or missing token");
+        }
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
