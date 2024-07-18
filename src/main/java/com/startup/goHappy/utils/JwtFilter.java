acbf67a6139230da56e5e08a6f0ea9f93a4601ec
mehul-gohappy <mehul.sharma@gohappyclub.in>
2024-07-18 22:33:44 +0530

    Resolve errors and add SecurityConfig
    Add role based authorizations in SecurityConfig for particular requests
    
    Remove unnecessary file
    
    Remove values from application.properties
    
    Allow callback URLs to be called without token

--- src/main/java/com/startup/goHappy/utils/JwtFilter.java
+++ src/main/java/com/startup/goHappy/utils/JwtFilter.java
@@ -26,7 +26,7 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        if (request.getServletPath().equals("/authenticate")) {
+        if (request.getServletPath().equals("/authenticate") || request.getServletPath().equals("/user/setPaymentDataWorkshop") || request.getServletPath().equals("/user/setPaymentDataContribution")) {
             filterChain.doFilter(request, response);
             return;
         }
