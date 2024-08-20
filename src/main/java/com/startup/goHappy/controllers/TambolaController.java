ac7779ccd02beee56eb476816246a886ab19ca9d
mehul1101 <mehul@drupsc.com>
2024-08-20 12:41:51 +0530

    Add crossing of already called numbers

--- src/main/java/com/startup/goHappy/controllers/TambolaController.java
+++ src/main/java/com/startup/goHappy/controllers/TambolaController.java
@@ -19,10 +19,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("tambola")
@@ -33,16 +31,18 @@ public class TambolaController {
     @ApiOperation(value = "get Tambola ticket by ticket number and event ID")
     @PostMapping("getTicket")
     public ResponseEntity<byte[]> getTambolaTicket(@RequestBody JSONObject params) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
         Optional<Event> oevent = eventService.findById(params.getString("eventId"));
         Event event = oevent.get();
         List<String> tickets = event.getTambolaTickets();
+        List<Integer> callingNumbers = event.getTambolaNumberCaller();
+        Map<String,Integer> liveTambola = event.getLiveTambola();
+        List<Integer> alreadyCalledNumbers = callingNumbers.subList(0, liveTambola.get("index"));
         String ticket = tickets.get(Integer.parseInt(params.getString("ticketNumber")));
         JSONObject output = new JSONObject();
         output.put("ticket", ticket);
         if (ticket != null) {
             int[][] ticketArray = parseTicketString(ticket);
-            BufferedImage image = createTicketImage(ticketArray);
+            BufferedImage image = createTicketImage(ticketArray,alreadyCalledNumbers);
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(image, "png", baos);
             byte[] imageBytes = baos.toByteArray();
@@ -82,15 +82,20 @@ public class TambolaController {
 
     @ApiOperation(value = "get List of already called numbers for tambola")
     @PostMapping("getCalledNumbers")
-    public JSONObject getCalledNumbers(@RequestBody JSONObject params) throws IOException {
+    public ResponseEntity<byte[]> getCalledNumbers(@RequestBody JSONObject params) throws IOException {
         Optional<Event> oevent = eventService.findById(params.getString("eventId"));
         Event event = oevent.get();
         List<Integer> callingNumbers = event.getTambolaNumberCaller();
         Map<String,Integer> liveTambola = event.getLiveTambola();
         List<Integer> alreadyCalledNumbers = callingNumbers.subList(0, liveTambola.get("index"));
-        JSONObject output = new JSONObject();
-        output.put("numbers",alreadyCalledNumbers);
-        return output;
+        BufferedImage image = createNumbersImage(alreadyCalledNumbers);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", baos);
+        byte[] imageBytes = baos.toByteArray();
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(imageBytes);
     }
 
     private int[][] parseTicketString(String ticketString) throws IOException {
@@ -110,7 +115,60 @@ public class TambolaController {
         return ticket;
     }
 
-    public BufferedImage createTicketImage(int[][] ticket) {
+    public BufferedImage createNumbersImage(List<Integer> alreadyCalled) {
+        int rows = 9;
+        int cols = 10;
+        int imageWidth = 650;
+        int imageHeight = 600;
+        int cellWidth = 60;
+        int cellHeight = 60;
+        int gap = 5;
+
+        int gridWidth = (cols * (cellWidth + gap)) - gap;
+        int gridHeight = (rows * (cellHeight + gap)) - gap;
+
+        int startX = (imageWidth - gridWidth) / 2;
+        int startY = (imageHeight - gridHeight) / 2;
+
+        BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = image.createGraphics();
+
+        g2d.setColor(Color.getHSBColor(5, 49, 100));
+        g2d.fillRect(0, 0, imageWidth, imageHeight);
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(Color.WHITE);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int x = startX + j * (cellWidth + gap);
+                int y = startY + i * (cellHeight + gap);
+                g2d.fillRoundRect(x, y, cellWidth, cellHeight, 15, 15);
+            }
+        }
+
+        g2d.setFont(new Font("Arial", Font.BOLD, 20));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int number = i * cols + j + 1;
+                String numberStr = String.valueOf(number);
+                int x = startX + j * (cellWidth + gap) + cellWidth / 2 - g2d.getFontMetrics().stringWidth(numberStr) / 2;
+                int y = startY + i * (cellHeight + gap) + cellHeight / 2 + g2d.getFontMetrics().getAscent() / 2 - 5;
+
+                if (alreadyCalled.contains(number)) {
+                    g2d.setColor(Color.BLACK);
+                } else {
+                    g2d.setColor(Color.LIGHT_GRAY);
+                }
+                g2d.drawString(numberStr, x, y);
+            }
+        }
+
+        g2d.dispose();
+        return image;
+    }
+
+    public BufferedImage createTicketImage(int[][] ticket, List<Integer> alreadyCalledNumbers) {
         int imageWidth = 550;
         int imageHeight = 180;
         int cellWidth = 50;
@@ -149,6 +207,13 @@ public class TambolaController {
                     int x = startX + j * (cellWidth + gap) + cellWidth / 2 - g2d.getFontMetrics().stringWidth(number) / 2;
                     int y = startY + i * (cellHeight + gap) + cellHeight / 2 + g2d.getFontMetrics().getAscent() / 2 - 5;
                     g2d.drawString(number, x, y);
+
+                    if (alreadyCalledNumbers.contains(ticket[i][j])) {
+                        g2d.setColor(Color.RED);
+                        g2d.setStroke(new BasicStroke(3));
+                        g2d.drawLine(x - cellWidth / 2 + 15, y - cellHeight / 2 , x + cellWidth / 2 +8, y + cellHeight / 2-10);
+                        g2d.setColor(Color.BLACK);
+                    }
                 }
             }
         }
