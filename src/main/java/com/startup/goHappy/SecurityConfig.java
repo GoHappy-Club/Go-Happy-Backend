2c7d33e750a704903425b746274075d09d2e8ee2
mehul1101 <mehul@drupsc.com>
2024-08-24 20:57:42 +0530

    Changes for tambola dashboard

--- src/main/java/com/startup/goHappy/SecurityConfig.java
+++ src/main/java/com/startup/goHappy/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/_ah/start").permitAll()
                 .antMatchers("/authenticate").permitAll()
