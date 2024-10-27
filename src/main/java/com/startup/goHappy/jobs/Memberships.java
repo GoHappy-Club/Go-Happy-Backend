9ed98bd4e62ac072419800465a5b991f79c50602
mehul1101 <mehul@drupsc.com>
2024-10-27 23:26:17 +0530

    Coins packages added with updated logic for coin top ups

--- src/main/java/com/startup/goHappy/jobs/Memberships.java
+++ src/main/java/com/startup/goHappy/jobs/Memberships.java
@@ -1,13 +1,18 @@
 package com.startup.goHappy.jobs;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Query;
 import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.gson.JsonObject;
+import com.startup.goHappy.controllers.UserProfileController;
 import com.startup.goHappy.entities.model.Membership;
 import com.startup.goHappy.entities.model.UserMemberships;
+import com.startup.goHappy.entities.model.UserProfile;
 import com.startup.goHappy.entities.repository.MembershipRepository;
 import com.startup.goHappy.entities.repository.UserMembershipsRepository;
 import com.startup.goHappy.enums.MembershipEnum;
@@ -15,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
@@ -29,18 +35,25 @@ public class Memberships {
 
     @Autowired
     MembershipRepository membershipRepository;
+    @Autowired
+    private UserProfileController userProfileController;
 
-//    @Scheduled(cron = "0 comme */6 * * *")
+    //    @Scheduled(cron = "0 0 */6 * * *")
     public void giveMonthlyCoins() throws ExecutionException, InterruptedException {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.MONTH, -1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
         long thirtyDaysAgoInMillis = calendar.getTimeInMillis();
 
         CollectionReference userMembershipRef = userMembershipsService.getCollectionReference();
         CollectionReference membershipRef = membershipRepository.getCollectionReference();
 
-        Query userQuery = userMembershipRef.whereLessThanOrEqualTo("lastCoinsCreditedDate", thirtyDaysAgoInMillis);
+        Query userQuery = userMembershipRef.whereEqualTo("lastCoinsCreditedDate", thirtyDaysAgoInMillis);
         ApiFuture<QuerySnapshot> querySnapshot = userQuery.get();
+        calendar.set(Calendar.MONTH, 1);
 
         for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
             UserMemberships userMembership = document.toObject(UserMemberships.class);
@@ -50,10 +63,27 @@ public class Memberships {
             for (DocumentSnapshot document1 : querySnapshot1.get().getDocuments()) {
                 Membership membership = document1.toObject(Membership.class);
                 userMembership.setCoins(userMembership.getCoins() + membership.getCoinsPerMonth());
+                userMembership.setLastCoinsCreditedDate("" + calendar.getTimeInMillis());
                 break;
             }
         }
     }
 
+    //    @Scheduled(cron = "0 0 */6 * * *")
+    public void notifyExpiringMembershipUsers() throws FirebaseMessagingException, ExecutionException, InterruptedException, IOException {
+        CollectionReference userMembershipRef = userMembershipsService.getCollectionReference();
 
+        long currTime = Calendar.getInstance().getTimeInMillis();
+        currTime = currTime + 50L * 24 * 60 * 60 * 1000;
+
+        Query userQuery = userMembershipRef.whereLessThanOrEqualTo("membershipEndDate", currTime);
+        ApiFuture<QuerySnapshot> querySnapshot = userQuery.get();
+        JSONObject getUserByPhoneParams = new JSONObject();
+        for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
+            UserMemberships userMembership = document.toObject(UserMemberships.class);
+            getUserByPhoneParams.put("phoneNumber",userMembership.getPhone());
+            JSONObject result = userProfileController.getUserByPhone(getUserByPhoneParams);
+            UserProfile user = result.getObject("user", UserProfile.class);
+        }
+    }
 }
