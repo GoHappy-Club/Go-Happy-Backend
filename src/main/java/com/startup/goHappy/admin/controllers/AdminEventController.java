197b819dae79b4c1b4e438cbbbfe4899708f5414
mehul1101 <mehul@drupsc.com>
2024-11-11 17:49:47 +0530

    Template creation and usage logic implemented

--- src/main/java/com/startup/goHappy/admin/controllers/AdminEventController.java
+++ src/main/java/com/startup/goHappy/admin/controllers/AdminEventController.java
@@ -1,16 +1,18 @@
 package com.startup.goHappy.admin.controllers;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Query;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.startup.goHappy.entities.model.Event;
+import com.startup.goHappy.entities.model.Templates;
 import com.startup.goHappy.entities.repository.EventRepository;
+import com.startup.goHappy.entities.repository.TemplateRepository;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.collections4.IterableUtils;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,6 +27,69 @@ import java.util.concurrent.ExecutionException;
 public class AdminEventController {
     @Autowired
     EventRepository eventService;
+    @Autowired
+    TemplateRepository templateService;
+
+    @ApiOperation(value = "To get already created templates")
+    @GetMapping("/getTemplates")
+    public List<Templates> getTemplates() {
+        Iterable<Templates> iterableTemplates = templateService.retrieveAll();
+        return IterableUtils.toList(iterableTemplates);
+    }
+
+    @ApiOperation(value = "To create a new params")
+    @PostMapping("/createTemplate")
+    public @ResponseBody JSONObject createTemplate(@RequestBody JSONObject params) {
+        Templates templateNew = new Templates();
+        templateNew.setId(UUID.randomUUID().toString());
+        if (params.containsKey("templateName")) {
+            templateNew.setTemplateName(params.getString("templateName"));
+        }if (params.containsKey("eventName")) {
+            templateNew.setEventName(params.getString("eventName"));
+        }if (params.containsKey("coverImage")) {
+            templateNew.setCoverImage(params.getString("coverImage"));
+        }if (params.containsKey("description")) {
+            templateNew.setDescription(params.getString("description"));
+        }if (params.containsKey("beautifulDescription")) {
+            templateNew.setBeautifulDescription(params.getString("beautifulDescription"));
+        }if (params.containsKey("expertImage")) {
+            templateNew.setExpertImage(params.getString("expertImage"));
+        }if (params.containsKey("expertName")) {
+            templateNew.setExpertName(params.getString("expertName"));
+        }if (params.containsKey("category")) {
+            templateNew.setCategory(params.getString("category"));
+        }if (params.containsKey("creator")) {
+            templateNew.setCreator(params.getString("creator"));
+        }if (params.containsKey("eventDate")) {
+            templateNew.setEventDate(params.getString("eventDate"));
+        }if (params.containsKey("startTime")) {
+            templateNew.setStartTime(params.getString("startTime"));
+        }if (params.containsKey("endTime")) {
+            templateNew.setEndTime(params.getString("endTime"));
+        }if (params.containsKey("costType")) {
+            templateNew.setCostType(params.getString("costType"));
+        }if (params.containsKey("cost")) {
+            templateNew.setCost(params.getInteger("cost"));
+        }if (params.containsKey("seatsLeft")) {
+            templateNew.setSeatsLeft(params.getInteger("seatsLeft"));
+        }if (params.containsKey("occurance")) {
+            templateNew.setOccurance(params.getString("occurance"));
+        }if (params.containsKey("type")) {
+            templateNew.setType(params.getString("type"));
+        }if (params.containsKey("isParent")) {
+            templateNew.setIsParent(params.getBoolean("isParent"));
+        }if (params.containsKey("isScheduled")) {
+            templateNew.setIsScheduled(params.getBoolean("isScheduled"));
+        }if (params.containsKey("cron")) {
+            templateNew.setCron(params.getString("cron"));
+        }if (params.containsKey("sameDayEventId")) {
+            templateNew.setSameDayEventId(params.getString("sameDayEventId"));
+        }
+        templateService.save(templateNew);
+        JSONObject output = new JSONObject();
+        output.put("success",true);
+        return output;
+    }
 
     @ApiOperation(value = "To get paginated events data")
     @GetMapping("getPaginatedEvents")
