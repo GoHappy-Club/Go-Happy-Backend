189d5d624330a93c1d7d676960c621b86cd93f9b
mehul1101 <mehul@drupsc.com>
2024-11-17 01:40:38 +0530

    voucher discounts in events and trips

--- src/main/java/com/startup/goHappy/entities/model/Vouchers.java
+++ src/main/java/com/startup/goHappy/entities/model/Vouchers.java
@@ -4,16 +4,24 @@ package com.startup.goHappy.entities.model;
 import com.startup.goHappy.enums.VoucherStatusEnum;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Data
 public class Vouchers {
     @DocumentId
     private String id;
     private String title;
     private String image;
-    private double value;
-    private int percent;
-    private double limit;
+    private Double value;
+    private Integer percent;
+    private Double limit;
     private String category;
-    private String description; //convert to map
+    private Map<String, Object> description = new HashMap<>() {{
+        put("redemption", new ArrayList<>(List.of("first first first first", "second second second second ")));
+        put("tnc", new ArrayList<>(List.of("first first first first", "second second second second ")));
+    }};
     private VoucherStatusEnum status;
 }
