df54416cd88a943be5835b46bfc3c697df396a0c
mehul1101 <mehul@drupsc.com>
2025-02-24 17:59:16 +0530

    Delay for webhook by 10 seconds and check if already executed the business logic

--- src/main/java/com/startup/goHappy/GoHappyApplication.java
+++ src/main/java/com/startup/goHappy/GoHappyApplication.java
@@ -2,6 +2,7 @@ package com.startup.goHappy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 @EnableEncryptableProperties
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class GoHappyApplication {
 
 	public static void main(String[] args) {
